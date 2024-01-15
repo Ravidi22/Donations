658d6bf5d321bd7041c03f55ff540bbcb70a809d
Ravidi22 <ravidrf22@gmail.com>
2024-01-15 21:17:31 +0200

    first commit - added foundations to the project

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
-export default function App() {
+// Tabs
+import PersonalDetailsTab from "./navigation/screens/PersonalDetailsTab";
+import MoneyTab from "./navigation/screens/MoneyTab";
+import RouteTab from "./navigation/screens/RouteDetailsTab";
+import SelfPositionTab from "./navigation/screens/SelfPositionTab";
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Route Details"
+          component={RouteTab}
+          options={{
+            tabBarLabel: "Route Details",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="route" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Self Position"
+          component={SelfPositionTab}
+          options={{
+            tabBarLabel: "Self Position",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="map" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Money"
+          component={MoneyTab}
+          options={{
+            tabBarLabel: "Personal",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="map" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Personal"
+          component={PersonalDetailsTab}
+          options={{
+            tabBarLabel: "Personal",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="person-circle-outline"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ navigation/screens/RouteDetailsTab.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RouteTab = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Route Tab</Text>
+    </View>
+  );
+};
+
+export default RouteTab;
